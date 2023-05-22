b09e716fc81292bdd8c7f3730e036543f5c50b44
Vangelis19 <90251115+Vangelis19@users.noreply.github.com>
2023-05-22 10:59:55 +0200

    Add files via upload

--- Match_report_pages.py
+++ Match_report_pages.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
+# Add the custom CSS styles
+st.markdown('<link href="https://github.com/Vangelis19/Streamlit/styles.css" rel="stylesheet">', unsafe_allow_html=True)
+
 def O4_D4(df):
     #O4 & D4-Total Dataframes
     Finishing = df[df['code'] == 'O4 Finishing']
@@ -19,20 +22,20 @@ def O4_D4(df):
     a_tr=Finishing[Finishing['01. Phase'] == 'Transition']
     a_tr = a_tr.reset_index(drop=True)
     a_sp=pd.DataFrame(columns=Finishing.columns)
-    for i in range(len(Finishing)):
-        if Finishing['01. Phase'][i]=='Throw In' or Finishing['01. Phase'][i]=='Lateral Free Kick' or Finishing['01. Phase'][i]=='Corner Kick' or Finishing['01. Phase'][i]=='Free Kick' or Finishing['01. Phase'][i]=='Penalty' or Finishing['01. Phase'][i]=='Direct Free Kick':
-            a_sp = pd.concat([a_sp, Finishing.iloc[i]])
     a_sp = a_sp.reset_index(drop=True)
+    for i in range(len(Finishing)):
+        if (Finishing['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            a_sp = a_sp.append(Finishing.iloc[i])
     #Defensive Phases Dataframes
     d_est=Defending[Defending['01. Phase'] == 'Established Play']
     d_est = d_est.reset_index(drop=True)
     d_tr=Defending[Defending['01. Phase'] == 'Transition']
     d_tr = d_tr.reset_index(drop=True)
     d_sp=pd.DataFrame(columns=Defending.columns)
-    for i in range(len(Defending)):
-        if Defending['01. Phase'][i]=='Throw In' or Defending['01. Phase'][i]=='Lateral Free Kick' or Defending['01. Phase'][i]=='Corner Kick' or Defending['01. Phase'][i]=='Free Kick' or Defending['01. Phase'][i]=='Penalty' or Defending['01. Phase'][i]=='Direct Free Kick':
-           d_sp = pd.concat([d_sp, Defending.iloc[i]])
     d_sp = d_sp.reset_index(drop=True)
+    for i in range(len(Defending)):
+        if (Defending['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            d_sp = d_sp.append(Defending.iloc[i])
 
     #Page1: 
     #Part 1: Finishing-Defending: Successful-Unsuccessful-Neutral:
@@ -95,118 +98,168 @@ def time_period(df):
     #0-15:
     Read_ep=Reading[Reading['01. Phase']=='Established Play']
     Read_ep = Read_ep.reset_index(drop=True)
+    EP0_15=Read_ep['01. Phase'].value_counts().get('Established Play', 0)
     Read_tr=Reading[Reading['01. Phase']=='Transition']
     Read_tr = Read_tr.reset_index(drop=True)
-    Read_sp=pd.DataFrame(columns=Finishing.columns)
-    for i in range(len(Reading)):
-        if Reading['01. Phase'][i]=='Throw In' or Reading['01. Phase'][i]=='Lateral Free Kick' or Reading['01. Phase'][i]=='Corner Kick' or Reading['01. Phase'][i]=='Free Kick' or Reading['01. Phase'][i]=='Penalty' or Reading['01. Phase'][i]=='Direct Free Kick':
-           Read_sp = pd.concat([Read_sp, Reading.iloc[i]])
+    TR0_15=Read_tr['01. Phase'].value_counts().get('Transition', 0)
+    Read_sp = pd.DataFrame(columns=Reacting.columns)
     Read_sp = Read_sp.reset_index(drop=True)
+    SP0_15 = 0
+
+    for i in range(len(Reading)):
+        if (Reading['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            Read_sp = Read_sp.append(Reading.iloc[i])
+            SP0_15 += 1
     #16-30:
     React_ep=Reacting[Reacting['01. Phase']=='Established Play']
     React_ep = React_ep.reset_index(drop=True)
+    EP16_30=React_ep['01. Phase'].value_counts().get('Established Play', 0)
     React_tr=Reacting[Reacting['01. Phase']=='Transition']
     React_tr = React_tr.reset_index(drop=True)
-    React_sp=pd.DataFrame(columns=Finishing.columns)
-    for i in range(len(Reacting)):
-        if Reacting['01. Phase'][i]=='Throw In' or Reacting['01. Phase'][i]=='Lateral Free Kick' or Reacting['01. Phase'][i]=='Corner Kick' or Reacting['01. Phase'][i]=='Free Kick' or Reacting['01. Phase'][i]=='Penalty' or Reacting['01. Phase'][i]=='Direct Free Kick':
-           React_sp = pd.concat([React_sp, Reacting.iloc[i]])
+    TR16_30=React_tr['01. Phase'].value_counts().get('Transition', 0)
+    React_sp = pd.DataFrame(columns=Reacting.columns)
     React_sp = React_sp.reset_index(drop=True)
+    SP16_30 = 0
+
+    for i in range(len(Reacting)):
+        if (Reacting['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            React_sp = React_sp.append(Reacting.iloc[i])
+            SP16_30 += 1
     #31-45:
     Act1_ep=Acting_1[Acting_1['01. Phase']=='Established Play']
     Act1_ep = Act1_ep.reset_index(drop=True)
+    EP31_45=Act1_ep['01. Phase'].value_counts().get('Established Play', 0)
     Act1_tr=Acting_1[Acting_1['01. Phase']=='Transition']
     Act1_tr = Act1_tr.reset_index(drop=True)
-    Act1_sp=pd.DataFrame(columns=Finishing.columns)
-    for i in range(len(Acting_1)):
-        if Acting_1['01. Phase'][i]=='Throw In' or Acting_1['01. Phase'][i]=='Lateral Free Kick' or Acting_1['01. Phase'][i]=='Corner Kick' or Acting_1['01. Phase'][i]=='Free Kick' or Acting_1['01. Phase'][i]=='Penalty' or Acting_1['01. Phase'][i]=='Direct Free Kick':
-           Act1_sp = pd.concat([Act1_sp, Acting_1.iloc[i]])
+    TR31_45=Act1_tr['01. Phase'].value_counts().get('Transition', 0)
+    Act1_sp = pd.DataFrame(columns=Acting_1.columns)
     Act1_sp = Act1_sp.reset_index(drop=True)
+    SP31_45 = 0
+
+    for i in range(len(Acting_1)):
+        if (Acting_1['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            Act1_sp = Act1_sp.append(Acting_1.iloc[i])
+            SP31_45 += 1
     #45+:
     Trp1_ep=Trap_1[Trap_1['01. Phase']=='Established Play']
     Trp1_ep = Trp1_ep.reset_index(drop=True)
+    EP45_=Trp1_ep['01. Phase'].value_counts().get('Established Play', 0)
     Trp1_tr=Trap_1[Trap_1['01. Phase']=='Transition']
     Trp1_tr = Trp1_tr.reset_index(drop=True)
-    Trp1_sp=pd.DataFrame(columns=Finishing.columns)
-    for i in range(len(Trap_1)):
-        if Trap_1['01. Phase'][i]=='Throw In' or Trap_1['01. Phase'][i]=='Lateral Free Kick' or Trap_1['01. Phase'][i]=='Corner Kick' or Trap_1['01. Phase'][i]=='Free Kick' or Trap_1['01. Phase'][i]=='Penalty' or Trap_1['01. Phase'][i]=='Direct Free Kick':
-           Trp1_sp = pd.concat([Trp1_sp, Trap_1.iloc[i]])
+    TR45_=Trp1_ep['01. Phase'].value_counts().get('Transition', 0)
+    Trp1_sp = pd.DataFrame(columns=Trap_1.columns)
     Trp1_sp = Trp1_sp.reset_index(drop=True)
+    SP45_ = 0
+
+    for i in range(len(Trap_1)):
+        if (Trap_1['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            Trp1_sp = Trp1_sp.append(Trap_1.iloc[i])
+            SP45_ += 1
     #46-60:
     Br_ep=Breathe[Breathe['01. Phase']=='Established Play']
     Br_ep = Br_ep.reset_index(drop=True)
+    EP46_60=Br_ep['01. Phase'].value_counts().get('Established Play', 0)
     Br_tr=Breathe[Breathe['01. Phase']=='Transition']
     Br_tr = Br_tr.reset_index(drop=True)
-    Br_sp=pd.DataFrame(columns=Finishing.columns)
-    for i in range(len(Breathe)):
-        if Breathe['01. Phase'][i]=='Throw In' or Breathe['01. Phase'][i]=='Lateral Free Kick' or Breathe['01. Phase'][i]=='Corner Kick' or Breathe['01. Phase'][i]=='Free Kick' or Breathe['01. Phase'][i]=='Penalty' or Breathe['01. Phase'][i]=='Direct Free Kick':
-           Br_sp = pd.concat([Br_sp, Breathe.iloc[i]])
+    TR46_60=Br_ep['01. Phase'].value_counts().get('Transition', 0)
+    Br_sp = pd.DataFrame(columns=Breathe.columns)
     Br_sp = Br_sp.reset_index(drop=True)
+    SP46_60 = 0
+
+    for i in range(len(Breathe)):
+        if (Breathe['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            Br_sp = Br_sp.append(Breathe.iloc[i])
+            SP45_60 += 1
     #61-75:
     Re_ep=Refresh[Refresh['01. Phase']=='Established Play']
     Re_ep = Re_ep.reset_index(drop=True)
+    EP61_75=Re_ep['01. Phase'].value_counts().get('Established Play', 0)
     Re_tr=Refresh[Refresh['01. Phase']=='Transition']
     Re_tr = Re_tr.reset_index(drop=True)
-    Re_sp=pd.DataFrame(columns=Finishing.columns)
-    for i in range(len(Refresh)):
-        if Refresh['01. Phase'][i]=='Throw In' or Refresh['01. Phase'][i]=='Lateral Free Kick' or Refresh['01. Phase'][i]=='Corner Kick' or Refresh['01. Phase'][i]=='Free Kick' or Refresh['01. Phase'][i]=='Penalty' or Refresh['01. Phase'][i]=='Direct Free Kick':
-           Re_sp = pd.concat([Re_sp, Refresh.iloc[i]])
+    TR61_75=Re_ep['01. Phase'].value_counts().get('Transition', 0)
+    Re_sp = pd.DataFrame(columns=Refresh.columns)
     Re_sp = Re_sp.reset_index(drop=True)
+    SP61_75 = 0
+
+    for i in range(len(Refresh)):
+        if (Refresh['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            Re_sp = Re_sp.append(Refresh.iloc[i])
+            SP61_75 += 1
     #76-90:
     Act2_ep=Acting_2[Acting_2['01. Phase']=='Established Play']
     Act2_ep = Act2_ep.reset_index(drop=True)
+    EP76_90=Act2_ep['01. Phase'].value_counts().get('Established Play', 0)
     Act2_tr=Acting_2[Acting_2['01. Phase']=='Transition']
     Act2_tr = Act2_tr.reset_index(drop=True)
-    Act2_sp=pd.DataFrame(columns=Finishing.columns)
-    for i in range(len(Acting_2)):
-        if Acting_2['01. Phase'][i]=='Throw In' or Acting_2['01. Phase'][i]=='Lateral Free Kick' or Acting_2['01. Phase'][i]=='Corner Kick' or Acting_2['01. Phase'][i]=='Free Kick' or Acting_2['01. Phase'][i]=='Penalty' or Acting_2['01. Phase'][i]=='Direct Free Kick':
-           Act2_sp = pd.concat([Act2_sp, Acting_2.iloc[i]])
+    TR76_90=Act2_tr['01. Phase'].value_counts().get('Transition', 0)
+    Act2_sp = pd.DataFrame(columns=Acting_2.columns)
     Act2_sp = Act2_sp.reset_index(drop=True)
+    SP76_90 = 0
+
+    for i in range(len(Acting_2)):
+        if (Acting_2['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            Act2_sp = Act2_sp.append(Acting_2.iloc[i])
+            SP76_90 += 1
     #90+:
     Trp2_ep=Trap_2[Trap_2['01. Phase']=='Established Play']
     Trp2_ep = Trp2_ep.reset_index(drop=True)
+    EP90_=Trp2_ep['01. Phase'].value_counts().get('Established Play', 0)
     Trp2_tr=Trap_2[Trap_2['01. Phase']=='Transition']
     Trp2_tr = Trp2_tr.reset_index(drop=True)
-    Trp2_sp=pd.DataFrame(columns=Finishing.columns)
-    for i in range(len(Trap_2)):
-        if Trap_2['01. Phase'][i]=='Throw In' or Trap_2['01. Phase'][i]=='Lateral Free Kick' or Trap_2['01. Phase'][i]=='Corner Kick' or Trap_2['01. Phase'][i]=='Free Kick' or Trap_2['01. Phase'][i]=='Penalty' or Trap_2['01. Phase'][i]=='Direct Free Kick':
-           Trp2_sp = pd.concat([Trp2_sp, Trap_2.iloc[i]])
+    TR90_=Trp2_ep['01. Phase'].value_counts().get('Transition', 0)
+    Trp2_sp = pd.DataFrame(columns=Trap_2.columns)
     Trp2_sp = Trp2_sp.reset_index(drop=True)
+    SP90_ = 0
+
+    for i in range(len(Trap_2)):
+        if (Trap_2['01. Phase'][i] in ['Throw In', 'Lateral Free Kick', 'Corner Kick', 'Free Kick', 'Penalty', 'Direct Free Kick']):
+            Trp2_sp = Trp2_sp.append(Trap_2.iloc[i])
+            SP90_ += 1
     #Page2:
-    EP0_15=Read_ep['04. Rating'].value_counts().get('Successful', 0)
-    TR0_15=Read_tr['04. Rating'].value_counts().get('Successful', 0)
-    SP0_15=Read_sp['04. Rating'].value_counts().get('Successful', 0)
+    SEP0_15=Read_ep['04. Rating'].value_counts().get('Successful', 0)
+    STR0_15=Read_tr['04. Rating'].value_counts().get('Successful', 0)
+    SSP0_15=Read_sp['04. Rating'].value_counts().get('Successful', 0)
 
-    EP16_30=React_ep['04. Rating'].value_counts().get('Successful', 0)
-    TR16_30=React_tr['04. Rating'].value_counts().get('Successful', 0)
-    SP16_30=React_sp['04. Rating'].value_counts().get('Successful', 0)
+    SEP16_30=React_ep['04. Rating'].value_counts().get('Successful', 0)
+    STR16_30=React_tr['04. Rating'].value_counts().get('Successful', 0)
+    SSP16_30=React_sp['04. Rating'].value_counts().get('Successful', 0)
+    
+    SEP31_45=Act1_ep['04. Rating'].value_counts().get('Successful', 0)
+    STR31_45=Act1_tr['04. Rating'].value_counts().get('Successful', 0)
+    SSP31_45=Act1_sp['04. Rating'].value_counts().get('Successful', 0)
 
-    EP31_45=Act1_ep['04. Rating'].value_counts().get('Successful', 0)
-    TR31_45=Act1_tr['04. Rating'].value_counts().get('Successful', 0)
-    SP31_45=Act1_sp['04. Rating'].value_counts().get('Successful', 0)
+    SEP45_=Trp1_ep['04. Rating'].value_counts().get('Successful', 0)
+    STR45_=Trp1_tr['04. Rating'].value_counts().get('Successful', 0)
+    SSP45_=Trp1_sp['04. Rating'].value_counts().get('Successful', 0)
 
-    EP45_=Trp1_ep['04. Rating'].value_counts().get('Successful', 0)
-    TR45_=Trp1_tr['04. Rating'].value_counts().get('Successful', 0)
-    SP45_=Trp1_sp['04. Rating'].value_counts().get('Successful', 0)
+    SEP46_60=Br_ep['04. Rating'].value_counts().get('Successful', 0)
+    STR46_60=Br_tr['04. Rating'].value_counts().get('Successful', 0)
+    SSP46_60=Br_sp['04. Rating'].value_counts().get('Successful', 0)
 
-    EP46_60=Br_ep['04. Rating'].value_counts().get('Successful', 0)
-    TR46_60=Br_tr['04. Rating'].value_counts().get('Successful', 0)
-    SP46_60=Br_sp['04. Rating'].value_counts().get('Successful', 0)
+    SEP61_75=Re_ep['04. Rating'].value_counts().get('Successful', 0)
+    STR61_75=Re_tr['04. Rating'].value_counts().get('Successful', 0)
+    SSP61_75=Re_sp['04. Rating'].value_counts().get('Successful', 0)
+    Re_tr
+    Re_sp
 
-    EP61_75=Re_ep['04. Rating'].value_counts().get('Successful', 0)
-    TR61_75=Re_tr['04. Rating'].value_counts().get('Successful', 0)
-    SP61_75=Re_sp['04. Rating'].value_counts().get('Successful', 0)
+    SEP76_90=Act2_ep['04. Rating'].value_counts().get('Successful', 0)
+    STR76_90=Act2_tr['04. Rating'].value_counts().get('Successful', 0)
+    SSP76_90=Act2_sp['04. Rating'].value_counts().get('Successful', 0)
 
-    EP76_90=Act2_ep['04. Rating'].value_counts().get('Successful', 0)
-    TR76_90=Act2_tr['04. Rating'].value_counts().get('Successful', 0)
-    SP76_90=Act2_sp['04. Rating'].value_counts().get('Successful', 0)
+    SEP90_=Trp2_ep['04. Rating'].value_counts().get('Successful', 0)
+    STR90_=Trp2_tr['04. Rating'].value_counts().get('Successful', 0)
+    SSP90_=Trp2_sp['04. Rating'].value_counts().get('Successful', 0)
 
-    EP90_=Trp2_ep['04. Rating'].value_counts().get('Successful', 0)
-    TR90_=Trp2_tr['04. Rating'].value_counts().get('Successful', 0)
-    SP90_=Trp2_sp['04. Rating'].value_counts().get('Successful', 0)
-
-    return (EP0_15,TR0_15,SP0_15,EP16_30,TR16_30,SP16_30,EP31_45,TR31_45,SP31_45,EP45_,TR45_,SP45_,EP46_60,TR46_60,SP46_60,EP61_75,TR61_75,
-            SP61_75,EP76_90,TR76_90,SP76_90,EP90_,TR90_,SP90_)
+    return (EP0_15,TR0_15,SP0_15,EP16_30,TR16_30,
+            SP16_30,EP31_45,TR31_45,SP31_45,EP45_,
+            TR45_,SP45_,EP46_60,TR46_60,SP46_60,
+            EP61_75,TR61_75,SP61_75,EP76_90,TR76_90,
+            SP76_90,EP90_,TR90_,SP90_,SEP0_15,
+            STR0_15,SSP0_15,SEP16_30,STR16_30,SSP16_30,
+            SEP31_45,STR31_45,SSP31_45,SEP45_,STR45_,
+            SSP45_,SEP46_60,STR46_60,SSP46_60,SEP61_75,
+            STR61_75,SSP61_75,SEP76_90,STR76_90,SSP76_90,
+            SEP90_,STR90_,SSP90_)
 
 
     
@@ -367,7 +420,7 @@ def statistics_page(df):
 def visualization_page(df):
     st.subheader("Quarters")
     st.write("This is the visualization page.")
-    EP0_15,TR0_15,SP0_15,EP16_30,TR16_30,SP16_30,EP31_45,TR31_45,SP31_45,EP45_,TR45_,SP45_,EP46_60,TR46_60,SP46_60,EP61_75,TR61_75,SP61_75,EP76_90,TR76_90,SP76_90,EP90_,TR90_,SP90_=time_period(df)
+    EP0_15,TR0_15,SP0_15,EP16_30,TR16_30,SP16_30,EP31_45,TR31_45,SP31_45,EP45_,TR45_,SP45_,EP46_60,TR46_60,SP46_60,EP61_75,TR61_75,SP61_75,EP76_90,TR76_90,SP76_90,EP90_,TR90_,SP90_,SEP0_15,STR0_15,SSP0_15,SEP16_30,STR16_30,SSP16_30,SEP31_45,STR31_45,SSP31_45,SEP45_,STR45_,SSP45_,SEP46_60,STR46_60,SSP46_60,SEP61_75,STR61_75,SSP61_75,SEP76_90,STR76_90,SSP76_90,SEP90_,STR90_,SSP90_=time_period(df)
     # Define the time periods
     time_periods = ['0-15', '16-30', '31-45', '45+', '46-60', '61-75', '76-90', '90+']
 
@@ -393,6 +446,8 @@ def visualization_page(df):
     # Set the positions of the bars on the x-axis
     bar_positions = np.arange(len(time_periods))
 
+    colors = ['purple', 'black', 'red']
+
     # Create a bar for each situation
     for i, situation in enumerate(situations):
         # Set the x-position for the bars of each situation
@@ -402,7 +457,7 @@ def visualization_page(df):
         counts = situation_counts[:, i]
 
         # Create the bar for the situation
-        bar = ax.bar(situation_positions, counts, bar_width, label=situation)
+        bar = ax.bar(situation_positions, counts, bar_width, label=situation, color=colors[i])
 
         # Connect the top points of bars for each situation with lines
         x_coords = [rect.get_x() + rect.get_width() / 2 for rect in bar]
